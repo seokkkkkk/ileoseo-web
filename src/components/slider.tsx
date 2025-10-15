a178023876eec44bd81108bc774ef4a2b51df56b
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-10-16 03:51:15 +0900

    fix: css 오류 수정

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -24,14 +24,14 @@ export default function Slider({ images }: { images: string[] }) {
             {/* 컨트롤 */}
             <button
                 onClick={prev}
-                className="absolute top-1/2 left-3 -translate-y-1/2 bg-white/80 rounded-full px-4 py-2shadow hover:bg-white"
+                className="absolute top-1/2 left-3 -translate-y-1/2 bg-white/80 rounded-full px-4 py-2 shadow hover:bg-white cursor-pointer"
                 aria-label="이전"
             >
                 ◀
             </button>
             <button
                 onClick={next}
-                className="absolute top-1/2 right-3 -translate-y-1/2 bg-white/80 rounded-full px-4 py-2 shadow hover:bg-white"
+                className="absolute top-1/2 right-3 -translate-y-1/2 bg-white/80 rounded-full px-4 py-2 shadow hover:bg-white cursor-pointer"
                 aria-label="다음"
             >
                 ▶
