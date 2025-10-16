816570d856d9a0b91417ab9b986e1f5872c5bcce
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-10-16 13:09:02 +0900

    chore: update configuration and dependencies; remove unused pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
         remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
+    },
+    turbopack: {
+        root: path.resolve(__dirname),
     },
 };
 
